d9698fb02e929fb184f03d965c2c40e812f4404e
Mark Fink <mark@mark-fink.de>
2012-06-23 11:16:44 +0200

    implemented basic command set

--- src/main/java/org/testingsoftware/selrunner/SelRunner.java
+++ src/main/java/org/testingsoftware/selrunner/SelRunner.java
@@ -1,24 +1,30 @@
 package org.testingsoftware.selrunner;
 
+import java.util.List;
+
 import org.apache.commons.lang.StringUtils;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.Select;
 
 import fitnesse.slim.converters.ConverterRegistry;
 //import fitnesse.slim.Slim;
 
 /**
- * Search Google example.
+ * Simple Webdriver adapter intended for Slim use.
  *
  */
 public class SelRunner {
-    static WebDriver webdriver;
-    static Wait<WebDriver> wait;
+    private static WebDriver webdriver;
+    private static Wait<WebDriver> wait;
+	private WebElement element;
 
     public SelRunner() {
         webdriver = new FirefoxDriver();
@@ -38,31 +44,102 @@ public class SelRunner {
     }
 
     public void open(String url) {
-        System.out.println("url: " + url);
+        //System.out.println("url: " + url);
 
         webdriver.get(url);
     }
 
-    public void click(By by) {
+    public void click() {
         // click link
         //webdriver.findElement(By.linkText("SelRunner")).click();
+        element.click();
+    }
+
+    public void click(By by) {
         webdriver.findElement(by).click();
     }
 
-    public boolean verifyTextPresent(String text) {
-        return webdriver.findElement(By.tagName("body")).getText().contains(
-            "The Hitchhickers Guide");
-    }
+	public boolean containsText(String text) {
+		return element.getText().contains(text);
+	}
 
+	public boolean findElement(By by) {
+		if (hasElement(by)) {
+			element = webdriver.findElement(by);
+			return true;
+		}
+		return false;
+	}
+
+	public boolean hasElement(By by) {
+		return numberOfElements(by) > 0;
+	}
+
+	public int numberOfElements(By by) {
+		return findElements(by).size();
+	}
+	
+	public List<WebElement> findElements(By by) {
+		return webdriver.findElements(by);
+	}
+	
     public String title() {
-        return getTitle();
-    }
-
-    public String getTitle() {
         return webdriver.getTitle();
     }
 
-    public void stop() {
+	public void selectByValue(String value) {
+		new Select(element).selectByValue(value);
+	}
+
+	public void selectByText(String text) {
+		new Select(element).selectByVisibleText(text);
+	}
+	
+	public void selectByIndex(int index) {
+		new Select(element).selectByIndex(index);
+	}
+
+	public void clear() {
+		element.clear();
+	}	
+
+	public void type(String keys) {
+		element.sendKeys(keys);
+	}
+
+	public void sendKeys(CharSequence[] keys) {
+		element.sendKeys(keys);
+	}
+
+	public void sendKey(Keys key) {
+		element.sendKeys(key);
+	}
+	
+	public void toggle() {
+		element.click();
+	}
+
+	public String text() {
+		return element.getText();
+	}
+
+	public boolean isSelected() {
+		return element.isSelected();
+	}
+
+	public void setSelected() {
+		if (!isSelected()) {
+			toggle();
+		}
+	}	
+
+	public void setUnselected() {
+		if (isSelected()) {
+			toggle();
+		}
+	}	
+
+	public void stop() {
         webdriver.close();
     }
 }
